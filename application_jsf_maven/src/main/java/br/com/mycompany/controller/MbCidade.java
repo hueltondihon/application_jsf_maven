2914499e20d0ef1f456201338e0f2b5e03fa3a9a
Huelton <hueltondihon@yahoo.com.br>
2013-11-21 20:37:48 -0200

    criacao do ManagedBean Cidade.

---
+++ application_jsf_maven/src/main/java/br/com/mycompany/controller/MbCidade.java
@@ -0,0 +1,87 @@
+package br.com.mycompany.controller;
+
+import br.com.mycompany.model.dao.HibernateDAO;
+import br.com.mycompany.model.dao.InterfaceDAO;
+import br.com.mycompany.model.entities.Cidade;
+import br.com.mycompany.util.FacesContextUtil;
+import java.io.Serializable;
+import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean(name = "mbCidade")
+@SessionScoped
+public class MbCidade implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    private Cidade cidade = new Cidade();
+    private List<Cidade> cidades;
+
+    public MbCidade() {
+    }
+    
+    private InterfaceDAO<Cidade> cidadeDAO(){
+       InterfaceDAO<Cidade> cidadeDAO = new HibernateDAO<Cidade>(Cidade.class, FacesContextUtil.getRequestSession()); 
+       return cidadeDAO;
+    }
+    
+    public String limpaCidade(){
+        cidade = new Cidade();
+        return "/restrict/cadastrarcidade.jsf";
+    }
+    
+    public String editCidade(){
+        return "/restrict/cadastrarcidade.jsf";
+    }
+    public String addCidade(){
+      if(cidade.getIdCidade() == null || cidade.getIdCidade() == 0 ){
+         insertCidade();
+      }else{
+         updateCidade();
+      }
+      limpaCidade();
+      return null;
+    }
+
+    private void insertCidade() {
+        cidadeDAO().save(cidade);
+        FacesContext.getCurrentInstance().addMessage(null, 
+            new FacesMessage(FacesMessage.SEVERITY_INFO, "Gravacao efetuada com sucesso!",""));
+    }
+
+    private void updateCidade() {
+        cidadeDAO().update(cidade);
+        FacesContext.getCurrentInstance().addMessage(null, 
+            new FacesMessage(FacesMessage.SEVERITY_INFO, "Atualizacao efetuada com sucesso!",""));  
+    }
+    
+    public void deletaCidade(){
+       cidadeDAO().remove(cidade);
+       FacesContext.getCurrentInstance().addMessage(null, 
+            new FacesMessage(FacesMessage.SEVERITY_INFO, "Deletado com sucesso!",""));
+    }
+    
+     public List<Cidade> getCidades() {
+        cidades = cidadeDAO().getEntities();
+        return cidades;
+    }
+
+    public void setCidades(List<Cidade> cidades) {
+        this.cidades = cidades;
+    }
+
+    public Cidade getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
+
+   
+    
+    
+}
