4b65f8e353ec1d75ef53d90480ce8a3f9b2ad623
Huelton <hueltondihon@yahoo.com.br>
2013-11-24 01:16:22 -0200

    Revert "Revert "criacao do projeto Sistema de Desktop""
    This reverts commit b8a443798075c1af00299959e1a6fb2f08dc0d95.

---
+++ Projeto_Sstema_Desktop/src/projeto_sstema_desktop/Projeto_Sstema_Desktop.java
@@ -0,0 +1,14 @@
+package projeto_sstema_desktop;
+
+import br.com.projetosistema.forms.JfrmPrincipal;
+
+
+public class Projeto_Sstema_Desktop {
+
+    
+    public static void main(String[] args) {
+        JfrmPrincipal frm = new JfrmPrincipal();
+        frm.setVisible(true);
+    }
+    
+}
